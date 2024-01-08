1d08c4b1f428c793bda00dc2756b9898d6988717
Jkvelagapudi <jkvelagapudi@gmail.com>
2024-01-08 14:58:58 -0600

    SignUp Button

--- AHS_Map/screens/SignUp.js
+++ AHS_Map/screens/SignUp.js
@@ -1,8 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button, Alert} from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
 export default function SignUp() {
     return (
       <View style={styles.container}>
+        <View>
         <StatusBar style="auto" />
         <Text style={styles.titlez}>Sign up</Text>
         <Text style={styles.titlem}>AHS Map and Availibilty</Text>
@@ -28,20 +29,26 @@ export default function SignUp() {
                         placeholder="Enter Password Again"
                         autoCorrect={false}
                         secureTextEntry={true}
-                        />
-                        <Button 
-        title="Press me"
-        color="#f194ff"
-        onPress={() => Alert.alert('Button with adjusted color pressed')}
-      />
-                        
+                        />    
+
+    </View>
+
+    <View>
+
+       <TouchableOpacity style={styles.button}> 
+          <Text> Sign Up </Text>
+        </TouchableOpacity>
+
+        </View>
+
       </View>
+      
     );
   }
   const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: 'black',
+      backgroundColor: '#F84F10',
       alignItems: 'center',
       justifyContent: 'top',
     },
@@ -52,52 +59,42 @@ export default function SignUp() {
         position: 'absolute',
         zIndex: 99,
         top: '27%',
-    left: '5%'
-       },
-       input: {
-        backgroundColor: 'white',
-        borderRadius: 10,
-        borderColor: 'black',
-        width: 350,
-        height: 80,
-        top: '18%'
-       },
-       input: {
-        backgroundColor: 'white',
-        borderRadius: 10,
-        borderColor: 'black',
-        width: 350,
-        height: 80,
-        margin: 20,
-        top: '18%'
+        left: '5%'
        },
        input: {
         backgroundColor: 'white',
         borderRadius: 20,
-        borderColor: 'black',
-        borderWidth: 8,
         width: 350,
         height: 80,
         margin: 20,
-        top: '18%'},
-        titlez: {
-          fontSize: 40,
-          top: '28%',
-          fontWeight: 'bold',
-          color: 'white'
+        top: '18%'
+      },
+      titlez: {
+        fontSize: 40,
+        top: '28%',
+        fontWeight: 'bold',
+        color: 'white',
+        alignSelf: "center"
+      },
+      titlem: {
+        marginTop: 16,
+        paddingVertical: 8,
+        borderWidth: 4,
+        borderColor: 'red',
+        borderRadius: 6,
+        color: 'white',
+        textAlign: 'center',
+        fontSize: 30,
+        fontWeight: 'bold',
+        fontStyle: 'italic'
         },
-        titlem: {
-          marginTop: 16,
-          paddingVertical: 8,
-          borderWidth: 4,
-          borderColor: 'red',
-          borderRadius: 6,
-          
-          color: 'white',
-          textAlign: 'center',
-          fontSize: 30,
-          fontWeight: 'bold',
-          fontStyle: 'italic'
-        }
-       
+       button: {
+        backgroundColor: "white",
+        width: 150,
+        alignItems: "center",
+        top: 100,
+        padding: 20,
+        borderRadius: 50,
+        backgroundColor: "#00BC9C"
+       }
   });
