2869f33999d463ca967a585278dfbda3c7826e08
Jkvelagapudi <jkvelagapudi@gmail.com>
2024-01-20 16:33:51 -0600

    minor fix

--- AHS_Map/navigation/TabNavigation.js
+++ AHS_Map/navigation/TabNavigation.js
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
 
 import Availibilty from "../screens/Availibilty";
