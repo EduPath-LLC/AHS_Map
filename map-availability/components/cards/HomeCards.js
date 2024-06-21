f2a5c095ea895ac989c2a8b8edd8d61cf0315a33
jkvelagapudi <jkvelagapudi@gmail.com>
2024-06-21 17:36:53 -0500

    Merge Broke Stuff so fixes

--- map-availability/components/cards/HomeCards.js
+++ map-availability/components/cards/HomeCards.js
@@ -10,81 +10,84 @@ export default class HomeCards extends Component {
         };
     }
 
-    componentDidMount() {
-        this.setState({ arr: this.props.info });
-    }
+    // componentDidMount() {
+    //     this.setState({ arr: this.props.info });
+    // }
 
-    handleInputChange = (id, value) => {
-        const newObject = { ...this.state.arr };
+    // handleInputChange = (id, value) => {
+    //     const newObject = { ...this.state.arr };
     
-        newObject[id] = value;
+    //     newObject[id] = value;
     
-        this.setState({ arr: newObject });
+    //     this.setState({ arr: newObject });
     
-        this.props.onInputChange(newObject);
-    };
+    //     this.props.onInputChange(newObject);
+    // };
     
 
     render() {
-        if(this.state.arr.id != 'Lunch'){
-            return (
+        <View>
+            <Text> Hello </Text>
+        </View>
+        // if(this.state.arr.id != 'Lunch'){
+        //     return (
 
-                <View style={styles.card}>
-                    <Text style={styles.period}> {this.state.arr.id} Period </Text>
+        //         <View style={styles.card}>
+        //             <Text style={styles.period}> {this.state.arr.id} Period </Text>
 
 
 
-                    <TextInput
-                        style={styles.input}
-                        placeholder="Class Name"
-                        value={this.state.arr.className}
-                        onChangeText={(value) => this.handleInputChange('className', value)}
-                    />
+        //             <TextInput
+        //                 style={styles.input}
+        //                 placeholder="Class Name"
+        //                 value={this.state.arr.className}
+        //                 onChangeText={(value) => this.handleInputChange('className', value)}
+        //             />
 
-                    <TextInput
-                        style={styles.input}
-                        placeholder="Teacher Last Name"
-                        value={this.state.arr.teacher}
-                        onChangeText={(value) => this.handleInputChange('teacher', value)}
-                    />
+        //             <TextInput
+        //                 style={styles.input}
+        //                 placeholder="Teacher Last Name"
+        //                 value={this.state.arr.teacher}
+        //                 onChangeText={(value) => this.handleInputChange('teacher', value)}
+        //             />
 
-                    <TextInput
-                        style={styles.input}
-                        placeholder="Building"
-                        value={this.state.arr.building}
-                        onChangeText={(value) => this.handleInputChange('building', value)}
-                    />
+        //             <TextInput
+        //                 style={styles.input}
+        //                 placeholder="Building"
+        //                 value={this.state.arr.building}
+        //                 onChangeText={(value) => this.handleInputChange('building', value)}
+        //             />
 
-                    <TextInput
-                        style={styles.input}
-                        placeholder="Room Number"
-                        value={this.state.arr.roomNumber}
-                        onChangeText={(value) => this.handleInputChange('roomNumber', value)}
-                    />
+        //             <TextInput
+        //                 style={styles.input}
+        //                 placeholder="Room Number"
+        //                 value={this.state.arr.roomNumber}
+        //                 onChangeText={(value) => this.handleInputChange('roomNumber', value)}
+        //             />
 
-                </View>
-            );
-        } else {
-            return(
-                <View style={styles.card}>
-                    <Text style={styles.period}> {this.state.arr.id} Period </Text>
+        //         </View>
+        //     );
+        // } else {
+        //     return(
+        //         <View style={styles.card}>
+        //             <Text style={styles.period}> {this.state.arr.id} Period </Text>
 
-                    <TextInput
-                        style={styles.input}
-                        placeholder="A Day Lunch"
-                        value={this.state.arr.a_day}
-                        onChangeText={(value) => this.handleInputChange('a_day', value)}
-                    />
+        //             <TextInput
+        //                 style={styles.input}
+        //                 placeholder="A Day Lunch"
+        //                 value={this.state.arr.a_day}
+        //                 onChangeText={(value) => this.handleInputChange('a_day', value)}
+        //             />
 
-                    <TextInput
-                        style={styles.input}
-                        placeholder="B Day Lunch"
-                        value={this.state.arr.b_day}
-                        onChangeText={(value) => this.handleInputChange('b_day', value)}
-                    />
+        //             <TextInput
+        //                 style={styles.input}
+        //                 placeholder="B Day Lunch"
+        //                 value={this.state.arr.b_day}
+        //                 onChangeText={(value) => this.handleInputChange('b_day', value)}
+        //             />
 
-                </View>
-            )
-        }
+        //         </View>
+        //     )
+        // }
     }
 }
